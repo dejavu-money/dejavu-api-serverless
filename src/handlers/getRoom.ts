5cbeb9eb7b5764eefaba688ec028feb89cd23bb2
Jorge Hernandez <jdaviderb@gmail.com>
2022-11-23 10:11:15 -0500

    fix readme

--- src/handlers/getRoom.ts
+++ src/handlers/getRoom.ts
@@ -1,9 +1,19 @@
 import { PublicKey } from "@solana/web3.js";
 import response from "../response";
 import { dejavu as dejavuProgram } from "../services/dejavu-program";
+import { sleep } from "../utils";
 
 export async function handler (event) {
-  const room = await dejavuProgram.getRoom(new PublicKey(event.pathParameters.id));
+  let retry = 0;
+  while (retry < 5) {
+    try {
+        const room = await dejavuProgram.getRoom(new PublicKey(event.pathParameters.id));
+        return response(room);
+      } catch (_) {
+        sleep(1000);
+        retry++;
+      }
+  }
   
-  return response(room);
+  return response({ message: 'not found' }, 404);
 }
